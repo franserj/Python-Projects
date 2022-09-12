574086ab9253e6c9657f11ce0be8ddf9e045ee9c
decastro <66888910+franserj@users.noreply.github.com>
2022-09-12 15:44:53 -0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import time
 def getWeather(canvas):
     city = textField.get()
     # https://home.openweathermap.org/
-    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06c921750b9a82d8f5d1294e1586276f"
+    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=KEY_API_ID"
 
     json_data = requests.get(api).json()
     condition = json_data['weather'][0]['main']
